7e8ce12691684866ab1982437ed18fc44bdbf652
geckoo <geckospaniel@gmail.com>
2022-11-04 00:39:47 +0200

    Clicking on instances can now set dependencies

--- site/index.js
+++ site/index.js
@@ -1,9 +1,11 @@
+const addTest = document.getElementById("test");
 const addTime = document.getElementById("time");
 const addDay = document.getElementById("day");
 const addDep = document.getElementById("addDep");
 const instances = document.getElementById("instances");
 
 const ws = new WebSocket("ws://localhost:3001");
+let dep = "";
 
 ws.addEventListener('message', (event) => {
 	const msg = JSON.parse(event.data.toString())
@@ -34,12 +36,38 @@ function sendMessage(json)
 	ws.send(JSON.stringify(json))
 }
 
+function addDependency(to, dep)
+{
+	let delim = dep.lastIndexOf(":");
+	let depID = dep.slice(0, delim);
+	let depInst = dep.slice(delim + 1);
+
+	delim = to.lastIndexOf(":");
+	let toID = to.slice(0, delim);
+	let toInst = to.slice(delim + 1);
+
+	sendMessage({
+		cmd: "dependency",
+		arg: [ toID, toInst, depID, depInst ]
+	})
+}
+
 function showInstance(id, index) {
 	let box = document.createElement("div");
 	box.className = "instance";
 	box.id = id + ":" + index.toString()
 
 	box.appendChild(document.createTextNode("ID: " + id + " index: " + index.toString()))
+	box.onclick = (e) => {
+		if(dep === "")
+			dep = e.target.id;
+
+		else
+		{
+			addDependency(dep, e.target.id);
+			dep = "";
+		}
+	}
 
 	instances.appendChild(box);
 }
@@ -68,5 +96,12 @@ addDay.onclick = () => {
 	sendMessage({
 		cmd: "instance",
 		arg: [ "day:1" ]
+	})
+}
+
+addTest.onclick = () => {
+	sendMessage({
+		cmd: "instance",
+		arg: [ "test:1" ]
 	})
 }
