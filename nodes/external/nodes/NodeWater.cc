e7ef28d1eee624e3a592e97bf21e4a0581c793f5
Kasperi Kiviluoma <72491928+ussaka@users.noreply.github.com>
2022-11-16 17:50:39 +0200

    Blink led if water is detected

--- nodes/external/nodes/NodeWater.cc
+++ nodes/external/nodes/NodeWater.cc
@@ -1,9 +1,21 @@
-#include <stdio.h>
+/*
+ * TODO: Make class and inherit BaseNode
+ * TODO: Sleep until water is detected?
+ */
+
+#include <cstdio>
 #include "pico/stdlib.h"
 #include "pico/binary_info.h"
 #include "hardware/i2c.h"
+#include "pico/cyw43_arch.h"
 
 int main() {
+    stdio_init_all();
+    if (cyw43_arch_init()) {
+        printf("WiFi init failed");
+        return -1;
+    }
+
 #if !defined(i2c_default) || !defined(PICO_DEFAULT_I2C_SDA_PIN) || !defined(PICO_DEFAULT_I2C_SCL_PIN)
 #warning i2c/bus_scan example requires a board with I2C pins
     puts("Default I2C pins were not defined");
@@ -16,14 +28,36 @@ int main() {
     gpio_pull_up(PICO_DEFAULT_I2C_SCL_PIN);
 
     // Make the I2C pins available to picotool
-    bi_decl(bi_2pins_with_func(PICO_DEFAULT_I2C_SDA_PIN, PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C));
+    bi_decl(bi_2pins_with_func(PICO_DEFAULT_I2C_SDA_PIN, PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C))
 
-    int addr = 0x78;
-    int ret;
-    uint8_t rxdata;
+    int attiny1_high_addr = 0x78;
+    int attiny2_low_addr = 0x77;
+    uint8_t data;
+
+    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, true);
+    sleep_ms(250);
+    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, false);
+    sleep_ms(250);
 
     while (true) {
-        ret = i2c_read_blocking(i2c_default, addr, &rxdata, 1, false);
+        for (int i = 0; i < 8; i++) {
+            i2c_read_blocking(i2c_default, attiny2_low_addr, &data, 1, true);
+            if (data > 0) {
+                cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, true);
+                sleep_ms(250);
+                cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, false);
+                sleep_ms(250);
+            }
+        }
+        for (int i = 0; i < 12; i++) {
+            i2c_read_blocking(i2c_default, attiny1_high_addr, &data, 1, true);
+            if (data > 0) {
+                cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, true);
+                sleep_ms(250);
+                cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, false);
+                sleep_ms(250);
+            }
+        }
     }
     return 0;
 #endif
