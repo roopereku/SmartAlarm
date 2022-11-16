141eb75d5814d14de5d87eb301928636e919ab51
Kasperi Kiviluoma <72491928+ussaka@users.noreply.github.com>
2022-11-16 15:00:07 +0200

    Initial commit

---
+++ nodes/external/nodes/NodeWater.cc
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "pico/binary_info.h"
+#include "hardware/i2c.h"
+
+int main() {
+#if !defined(i2c_default) || !defined(PICO_DEFAULT_I2C_SDA_PIN) || !defined(PICO_DEFAULT_I2C_SCL_PIN)
+#warning i2c/bus_scan example requires a board with I2C pins
+    puts("Default I2C pins were not defined");
+#else
+    // This example will use I2C0 on the default SDA and SCL pins (GP4, GP5 on a Pico)
+    i2c_init(i2c_default, 100 * 1000);
+    gpio_set_function(PICO_DEFAULT_I2C_SDA_PIN, GPIO_FUNC_I2C);
+    gpio_set_function(PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C);
+    gpio_pull_up(PICO_DEFAULT_I2C_SDA_PIN);
+    gpio_pull_up(PICO_DEFAULT_I2C_SCL_PIN);
+
+    // Make the I2C pins available to picotool
+    bi_decl(bi_2pins_with_func(PICO_DEFAULT_I2C_SDA_PIN, PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C));
+
+    int addr = 0x78;
+    int ret;
+    uint8_t rxdata;
+
+    while (true) {
+        ret = i2c_read_blocking(i2c_default, addr, &rxdata, 1, false);
+    }
+    return 0;
+#endif
+}
+
