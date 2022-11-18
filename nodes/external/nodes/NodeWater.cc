880071aa74dcb227cc781d626587310ba22f1c5d
Kasperi Kiviluoma <72491928+ussaka@users.noreply.github.com>
2022-11-18 13:16:29 +0200

    Water detection working - Change NodeContext to Sensor and add 100 delay - Blink LED if water is detected

--- nodes/external/nodes/NodeWater.cc
+++ nodes/external/nodes/NodeWater.cc
@@ -11,7 +11,7 @@
 
 class NodeWater : public NodeBase {
 public:
-    NodeWater(const char *name) : NodeBase("water", name, NodeContext::Action, 0) {
+    NodeWater(const char *name) : NodeBase("water", name, NodeContext::Sensor, 100) {
         // This example will use I2C0 on the default SDA and SCL pins (GP4, GP5 on a Pico)
         i2c_init(i2c_default, 100 * 1000);
         gpio_set_function(PICO_DEFAULT_I2C_SDA_PIN, GPIO_FUNC_I2C);
@@ -26,16 +26,29 @@ public:
     bool check(Params &params) override {
         int attiny1_high_addr = 0x78;
         int attiny2_low_addr = 0x77;
-        uint8_t data;
+        uint8_t result;
 
         for (int i = 0; i < 8; i++) {
-            i2c_read_blocking(i2c_default, attiny2_low_addr, &data, 1, true);
+            i2c_read_blocking(i2c_default, attiny2_low_addr, &result, 1, true);
+            if (result > 0) {
+                cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, true);
+                sleep_ms(250);
+                cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, false);
+                sleep_ms(250);
+                return true;
+            }
         }
         for (int i = 0; i < 12; i++) {
-            i2c_read_blocking(i2c_default, attiny1_high_addr, &data, 1, true);
+            i2c_read_blocking(i2c_default, attiny1_high_addr, &result, 1, true);
+            if (result > 0) {
+                cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, true);
+                sleep_ms(250);
+                cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, false);
+                sleep_ms(250);
+                return true;
+            }
         }
-        auto &cmp = params["comparison"];
-        return cmp == ">" ? data >= 0 : data < 1;
+        return false;
     }
 };
 
