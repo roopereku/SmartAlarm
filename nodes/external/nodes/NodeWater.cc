342de67c962697532bebf509dd0a4dc2d9f23bb7
Kasperi Kiviluoma <72491928+ussaka@users.noreply.github.com>
2022-11-18 10:55:03 +0200

    Make NodeWater class For now removed sleeping

--- nodes/external/nodes/NodeWater.cc
+++ nodes/external/nodes/NodeWater.cc
@@ -7,60 +7,41 @@
 #include "pico/stdlib.h"
 #include "pico/binary_info.h"
 #include "hardware/i2c.h"
-#include "pico/cyw43_arch.h"
+#include "NodeBase.hh"
 
-int main() {
-    stdio_init_all();
-    if (cyw43_arch_init()) {
-        printf("WiFi init failed");
-        return -1;
+class NodeWater : public NodeBase {
+public:
+    NodeWater(const char *name) : NodeBase("water", name, NodeContext::Action, 0) {
+        // This example will use I2C0 on the default SDA and SCL pins (GP4, GP5 on a Pico)
+        i2c_init(i2c_default, 100 * 1000);
+        gpio_set_function(PICO_DEFAULT_I2C_SDA_PIN, GPIO_FUNC_I2C);
+        gpio_set_function(PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C);
+        gpio_pull_up(PICO_DEFAULT_I2C_SDA_PIN);
+        gpio_pull_up(PICO_DEFAULT_I2C_SCL_PIN);
+
+        // Make the I2C pins available to picotool
+        bi_decl(bi_2pins_with_func(PICO_DEFAULT_I2C_SDA_PIN, PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C))
     }
 
-#if !defined(i2c_default) || !defined(PICO_DEFAULT_I2C_SDA_PIN) || !defined(PICO_DEFAULT_I2C_SCL_PIN)
-#warning i2c/bus_scan example requires a board with I2C pins
-    puts("Default I2C pins were not defined");
-#else
-    // This example will use I2C0 on the default SDA and SCL pins (GP4, GP5 on a Pico)
-    i2c_init(i2c_default, 100 * 1000);
-    gpio_set_function(PICO_DEFAULT_I2C_SDA_PIN, GPIO_FUNC_I2C);
-    gpio_set_function(PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C);
-    gpio_pull_up(PICO_DEFAULT_I2C_SDA_PIN);
-    gpio_pull_up(PICO_DEFAULT_I2C_SCL_PIN);
+    bool check(Params &params) override {
+        int attiny1_high_addr = 0x78;
+        int attiny2_low_addr = 0x77;
+        uint8_t data;
 
-    // Make the I2C pins available to picotool
-    bi_decl(bi_2pins_with_func(PICO_DEFAULT_I2C_SDA_PIN, PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C))
-
-    int attiny1_high_addr = 0x78;
-    int attiny2_low_addr = 0x77;
-    uint8_t data;
-
-    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, true);
-    sleep_ms(250);
-    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, false);
-    sleep_ms(250);
-
-    while (true) {
         for (int i = 0; i < 8; i++) {
             i2c_read_blocking(i2c_default, attiny2_low_addr, &data, 1, true);
-            if (data > 0) {
-                cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, true);
-                sleep_ms(250);
-                cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, false);
-                sleep_ms(250);
-            }
         }
         for (int i = 0; i < 12; i++) {
             i2c_read_blocking(i2c_default, attiny1_high_addr, &data, 1, true);
-            if (data > 0) {
-                cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, true);
-                sleep_ms(250);
-                cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, false);
-                sleep_ms(250);
-            }
         }
-        sleep_ms(10000); // 10 seconds
+        auto &cmp = params["comparison"];
+        return cmp == ">" ? data >= 0 : data < 1;
     }
-    return 0;
-#endif
+};
+
+int main() {
+    NodeWater node("water1");
+    node.run();
 }
+
 
