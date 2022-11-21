89b52014187e9bd5093304296368f74a9c04df05
mustafan <m_nawrozi@hotmail.co.uk>
2022-11-21 14:04:43 +0200

    Telegram has been added for sending messages

---
+++ nodes/builtin/NodeTelegram.py
@@ -0,0 +1,37 @@
+import os
+import requests
+from NodeBase import node_base
+from dotenv import load_dotenv
+
+def send_to_telegram(message):
+
+    chatID = '1999030716'
+    apiURL = f'https://api.telegram.org/bot{apiToken}/sendMessage'
+
+    try:
+        response = requests.post(apiURL, json={'chat_id': chatID, 'text': message})
+        print(response.text)
+    except Exception as e:
+        print(e)
+
+class node_telegram(node_base):
+    def activate(self, params):
+        send_to_telegram(params["message"])
+
+
+    def deactivate(self, params):
+        pass
+
+    def validate_params(self, params):
+        pass
+
+    def get_params_format(self):
+        return {
+            "message": {
+            "type": "text"
+
+            }
+        }
+load_dotenv()
+apiToken = os.getenv('TELEGRAM_TOKEN')
+node = node_telegram("telegram", 0.5, "action")
