0e5e9383d616ab3db3ccc9e8c86d3f3a59a3e826
Kasperi <k.kiviluoma@pm.me>
2022-12-15 14:31:22 +0200

    Send only one email between activation and deactivation

--- nodes/builtin/NodeEmail.py
+++ nodes/builtin/NodeEmail.py
@@ -7,6 +7,10 @@ from dotenv import load_dotenv
 
 from NodeBase import node_base
 
+# Send only one email after activation
+# After activation set to false
+# After deactivation set to true
+rdy_to_send = True
 
 class NodeEmail(node_base):
     """
@@ -22,31 +26,33 @@ class NodeEmail(node_base):
     def activate(self, params):
         """
         Sends an email when activated
+
         """
+        global rdy_to_send
 
-        port = 465  # For SSL
-        smtp_server = "smtp.gmail.com"
-        sender_email = "smarttiaalarmia@gmail.com"
-        # Password is read from .env file
-        load_dotenv()
-        password = os.getenv('EMAIL_PASSWORD')
+        if rdy_to_send:
+            rdy_to_send = False
+            port = 465  # For SSL
+            smtp_server = "smtp.gmail.com"
+            sender_email = "smarttiaalarmia@gmail.com"
+            # Password is read from .env file
+            load_dotenv()
+            password = os.getenv('EMAIL_PASSWORD')
 
-        msg = EmailMessage()
-        msg.set_content(params["body"])
-        msg['Subject'] = params["header"]
-        msg['From'] = sender_email
-        msg['To'] = params["receiver"]
+            msg = EmailMessage()
+            msg.set_content(params["body"])
+            msg['Subject'] = params["header"]
+            msg['From'] = sender_email
+            msg['To'] = params["receiver"]
 
-        context = ssl.create_default_context()
-        with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-            server.login(sender_email, password)
-            server.send_message(msg, from_addr=sender_email, to_addrs=params["receiver"])
+            context = ssl.create_default_context()
+            with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+                server.login(sender_email, password)
+                server.send_message(msg, from_addr=sender_email, to_addrs=params["receiver"])
 
     def deactivate(self, params):
-        """
-        No need to deactivate
-        """
-        pass
+        global rdy_to_send
+        rdy_to_send = True
 
     def validate_params(self, params):
         """
@@ -71,4 +77,4 @@ class NodeEmail(node_base):
         }
 
 
-node = NodeEmail("email", 1, "action", "fa-regular fa-envelope")
+node = NodeEmail("email", 0, "action", "fa-regular fa-envelope")
